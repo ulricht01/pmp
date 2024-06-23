9c84e8f7bd1dc069dea8e8e29c007558a2d0a045
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2024-06-23 20:56:41 +0200

    Update index.tsx

--- pmp/app/(tabs)/index.tsx
+++ pmp/app/(tabs)/index.tsx
@@ -1,46 +1,15 @@
-import { Image, StyleSheet, Platform, Button, Text, View } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
 import React, {useState, useEffect} from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
-  const [time, setTime] = useState(0);
-  const [intervalId, setIntervalId] = useState<number | NodeJS.Timeout | null>(null);
-
-  const startTimer = () => {
-    if (intervalId) return;
-    const id = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    },1000);
-    setIntervalId(id);
-  };
-
-  const stopTimer = () => {
-    if (intervalId) {
-      clearInterval(intervalId as NodeJS.Timeout);
-      setIntervalId(null);
-      setTime(0);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      if(intervalId) clearInterval(intervalId);
-    };
-  }, [intervalId]);
-
-
   return (
     <View style={styles.container}>
-      <Text>{time} sekund</Text>
-      <View style={styles.buttonContainer}>
-      <Button title="Start" onPress={startTimer}/>
-      <Button title="Stop" onPress={stopTimer} />
-      </View>
+      <Text style={styles.mainText}>McTaskFace</Text>
+      <Image src=''></Image>
     </View>
   );
 }
@@ -50,11 +19,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'rgba(252, 255, 168, 0.8)'
   },
-  buttonContainer: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    gap: 10,    
-  },
+  mainText: {
+    color: 'rgba(11, 11, 9, 0.8)',
+    fontSize: 25,
+    textShadowColor: 'rgba(255, 62, 62, 0.8)',
+    textShadowOffset: {width: 0, height: -20},
+    textShadowRadius: 2,
+    fontFamily: 'Courier',
+    bottom:100,
+    fontStyle: 'italic',
+    fontWeight: 'bold'
+  }
 });
